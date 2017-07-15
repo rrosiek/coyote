5ccb4ad5db97ef1af4fb41ababe4f18c04883723
Ryan Rosiek <ryan.rosiek@gmail.com>
2017-07-14 20:38:26 -0400

    updating laravel mix config changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,6 @@ const { mix } = require('laravel-mix')
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.sass', 'public/css')
-    .copy('resources/assets/images/*', 'public/images')
-    .copy('resources/assets/favicon/*', 'public')
+    .copyDirectory('resources/assets/images', 'public/images')
+    .copyDirectory('resources/assets/favicon', 'public')
     // .browserSync({ proxy: 'coyote.app' })
